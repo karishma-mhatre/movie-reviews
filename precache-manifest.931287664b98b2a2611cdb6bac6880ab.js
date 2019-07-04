519eb5f10a37c00b26d54d3107c747419d5e6ceb
Karishma Mhatre <karishmamhatre077@gmail.com>
2019-07-04 15:56:33 +0530

    Updates

---
+++ precache-manifest.931287664b98b2a2611cdb6bac6880ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b885ca967a2b7f9652f9e62b2507678",
+    "url": "/movie-reviews/index.html"
+  },
+  {
+    "revision": "2e67895ad70a7ea6b0c1",
+    "url": "/movie-reviews/static/css/main.c13336f5.chunk.css"
+  },
+  {
+    "revision": "2ffe9de745c87d880c41",
+    "url": "/movie-reviews/static/js/2.fb71b4a8.chunk.js"
+  },
+  {
+    "revision": "2e67895ad70a7ea6b0c1",
+    "url": "/movie-reviews/static/js/main.c27154ad.chunk.js"
+  },
+  {
+    "revision": "6397ad7c65022e8ab55f",
+    "url": "/movie-reviews/static/js/runtime~main.c07caec6.js"
+  }
+]);
